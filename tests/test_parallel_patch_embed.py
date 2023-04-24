0d8a81b5fbfc05d9b630a4bf94c9c6deb2c5ddfd
rejuvyesh <mail@rejuvyesh.com>
2023-04-24 02:50:03 +0000

    add more tests

--- tests/test_parallel_patch_embed.py
+++ tests/test_parallel_patch_embed.py
@@ -14,19 +14,22 @@ def test_parallel_patch_embed():
         serial_model.token_embeds[i].proj.weight.data = parallel_model.token_embeds.proj_weights[i].data
         serial_model.token_embeds[i].proj.bias.data = parallel_model.token_embeds.proj_biases[i].data
 
-    serial_var_ids = serial_model.get_var_ids(vars, x.device)
-    parallel_var_ids = parallel_model.get_var_ids(vars, x.device)
-    assert all(serial_var_ids == parallel_var_ids)
+    test_vars = [["a"], ["b"], ["c"], ["a", "b"], ["a", "c"], ["b", "c"], ["a", "b", "c"]]
+    for vars in test_vars:
+        vars = tuple(vars)
+        serial_var_ids = serial_model.get_var_ids(vars, x.device)
+        parallel_var_ids = parallel_model.get_var_ids(vars, x.device)
+        assert all(serial_var_ids == parallel_var_ids)
+        x = torch.rand(4, len(vars), 32, 64)
+        parallel_embed = parallel_model.token_embeds(x, parallel_var_ids)
+        embeds = []
+        for i in range(len(serial_var_ids)):
+            id = serial_var_ids[i]
+            embeds.append(serial_model.token_embeds[id](x[:, i : i + 1, :, :]))
 
-    parallel_embed = parallel_model.token_embeds(x, parallel_var_ids)
-    embeds = []
-    for i in range(len(serial_var_ids)):
-        id = serial_var_ids[i]
-        embeds.append(serial_model.token_embeds[id](x[:, i : i + 1, :, :]))
-
-    serial_embed = torch.stack(embeds, dim=1)
-    assert parallel_embed.shape == serial_embed.shape
-    assert torch.allclose(serial_embed, parallel_embed)
+        serial_embed = torch.stack(embeds, dim=1)
+        assert parallel_embed.shape == serial_embed.shape
+        assert torch.allclose(serial_embed, parallel_embed)
 
 
 if __name__ == "__main__":
